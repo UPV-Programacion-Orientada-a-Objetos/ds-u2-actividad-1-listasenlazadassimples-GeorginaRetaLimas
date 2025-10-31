a167739e2396cb7d42fc4ec0dbf4c3b36a6b63d7
GeorginaRetaLimas <2430043@upv.edu.mx>
2025-10-30 23:07:00 -0600

    Commit antes de ejecutar el codigo pipipipi

---
+++ build/CMakeFiles/SistemaIoT.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gina/Documentos/EstDatos/Actividades/U2-Act1-listasenlazadas/src/main.cpp" "CMakeFiles/SistemaIoT.dir/src/main.cpp.o" "gcc" "CMakeFiles/SistemaIoT.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SistemaIoT.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SistemaIoT.dir/src/main.cpp.o"
+  "CMakeFiles/SistemaIoT.dir/src/main.cpp.o.d"
+  "SistemaIoT"
+  "SistemaIoT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SistemaIoT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
